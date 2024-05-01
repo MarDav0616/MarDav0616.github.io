c195ed81166163e88cf19b9622e621fe3785a1b0
MarDav0616 <159290678+MarDav0616@users.noreply.github.com>
2024-05-01 08:19:07 +0200

    Update main.jsx

--- Quinta/React/Routing/main.jsx
+++ Quinta/React/Routing/main.jsx
@@ -1,1 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Page from './Page.jsx';
+import Homepage from './Homepage.jsx';
 
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Homepage />,
+      children: [
+        {
+          path: "/page",
+          element: <Page />
+        },
+      ],
+    },
+  ]);
+  return (
+    <RouterProvider router={router} />
+  );
+};
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
